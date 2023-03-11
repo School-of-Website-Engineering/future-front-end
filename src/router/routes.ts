6b4dc6c8645631fd8dd6f90047b0ead638bd0380
mason369 <1960638223@qq.com>
2023-03-11 16:35:46 +0800

    修改路由

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,30 +3,15 @@ import { RouteRecordRaw } from 'vue-router';
 
 export const routes: Array<RouteRecordRaw> = [
     // 重定向
-    // {
-    //     path    : '/',
-    //     redirect: '/login'
-    // },
-    // 登录
-    // 登录
     {
-        path     : '/login',
-        name     : 'login',
-        component: () => import('../views/Login/index.vue')
+        path    : '/',
+        redirect: '/home'
     },
     {
         // 主页
         path     : '/home',
         name     : 'home',
-        component: () => import('../components/Home/index.vue'),
-        children : [
-            // 首页
-            {
-                path     : '/index',
-                name     : 'index',
-                component: () => import('../components/Home/index.vue')
-            }
-        ]
+        component: () => import('../views/Home/index.vue')
     },
     //     404
     {
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>首页</div>
+    <div>主页</div>
 </template>
 
 <script setup lang="ts"></script>
