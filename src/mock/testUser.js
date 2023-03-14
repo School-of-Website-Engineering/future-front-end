e4aadd6593ceee4907da5f38ac3dc0ed6109bbb7
mason369 <1960638223@qq.com>
2023-03-14 08:32:26 +0800

    update:添加mock登录接口

---
+++ src/mock/testUser.js
@@ -0,0 +1,19 @@
+export default [
+    // 用户登录
+    {
+        // 请求地址
+        url     : '/user/login',
+        // 请求方法
+        method  : 'post',
+        // 响应数据
+        response: () => {
+            return {
+                code   : 0,
+                message: 'success',
+                data   : {
+                    token: 'Tokenasdfasdfasdfasdfasdfasd'
+                }
+            };
+        }
+    }
+];
