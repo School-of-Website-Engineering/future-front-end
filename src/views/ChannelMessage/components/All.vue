b6cc8a9c8bf165a8ed86acedb7e6c6b7ce2ffb03
mason369 <1960638223@qq.com>
2023-03-27 17:16:23 +0800

    fix:修复好友展示框bug，使用sticky替换fixed

--- src/views/ChannelMessage/components/All.vue
+++ src/views/ChannelMessage/components/All.vue
@@ -5,15 +5,13 @@
 <script setup lang="ts">
 import { defineComponent } from 'vue';
 import { useUserFriendsStore } from '@/store/modules/friends';
+
 const userFriends = useUserFriendsStore();
 
 defineComponent({
     name: 'All'
 });
 const search = ref('');
-const searchFriend = () => {
-    console.log(search);
-};
 </script>
 
 <style lang="scss" scoped></style>
