01b57c233ed7c395333bdb6359bcdb9ccc17fef7
mason369 <1960638223@qq.com>
2023-04-28 11:02:25 +0800

    update：更新样式

--- src/views/Chat/components/common/AsideRight.vue
+++ src/views/Chat/components/common/AsideRight.vue
@@ -8,7 +8,7 @@ import router from '@/router';
 
 const friendsStore = useUserFriendsStore();
 // 用户信息在friendsStore.friendInfo中，直接调用即可
-// 使用watch监听路由变化
+// 使用watch监听路由变化，获取当前路由的id，然后调用friendsStore.getFriendInfo(id)方法获取用户信息
 watch(
     () => router.currentRoute.value.params.id,
     (id) => {
