bf5ba432982fe4be2cc4d892e596a89ba1d0b5c4
LingASDJ <2735951230@qq.com>
2023-05-04 14:57:29 +0800

    Update：进一步完善页面

--- src/views/Chat/components/common/AsideRight.vue
+++ src/views/Chat/components/common/AsideRight.vue
@@ -1,24 +1,12 @@
 <template>
     <div class="main-message" style="overflow: hidden scroll; padding-right: 0">
-        <foreignObject x="0" y="0" width="100%" height="100%" overflow="visible" mask="url(#uid_38)">
-            <div class="panelBanner bannerPremium" style="background-color: #1f2123">
-                <svg width="92" height="92" viewBox="0 0 92 92" class="mask" aria-hidden="true">
-                    <foreignObject x="0" y="0" width="100" height="100" mask="url(round-80)">
-                        <div class="avatar-container">
-                            <img class="avatar-image"
-                                 src="https://cdn.discordapp.com/avatars/760729927552729119/20167f95c93ef46167737aee7201cb92.webp?size=48"
-                                 alt="">
-                        </div>
-                    </foreignObject>
-                    <rect width="16" rx="30" ry="30" height="16" x="63" y="65" fill="#23a55a" mask="url(round-80)">
-                    </rect>
-                </svg>
-            </div>
-        </foreignObject>
         <div class="badgeList">
             <a @click="openVn" class="anchorUnderlineOnHover">
-                <img alt="Null" class="imgStyle"
-                     src="https://cdn.discordapp.com/badge-icons/3aa41de486fa12454c3761e8e223442e.png">
+                <img
+                    alt="Null"
+                    class="imgStyle"
+                    src="https://cdn.discordapp.com/badge-icons/3aa41de486fa12454c3761e8e223442e.png"
+                />
             </a>
         </div>
         <div class="right-card">
@@ -29,10 +17,10 @@
             <div class="line"></div>
             <h2 class="userDes">自我介绍</h2>
             <div style="-webkit-line-clamp: 6">
-            <span class="userText"
-            >我想和你在一起，无须遨游远方，只要执手相依，<br/>到处都是风景： 我们在一起，不必锦衣玉食，<br/>哪怕粗布淡饭，亦是阳光满天：
-                我们在一起，<br/>远离曲折是非，领略风平浪静，平淡也是幸福。<br/>
-            </span>
+                <span class="userText"
+                    >我想和你在一起，无须遨游远方，只要执手相依，<br />到处都是风景： 我们在一起，不必锦衣玉食，<br />哪怕粗布淡饭，亦是阳光满天：
+                    我们在一起，<br />远离曲折是非，领略风平浪静，平淡也是幸福。<br />
+                </span>
             </div>
             <div class="line"></div>
             <h2 class="userDes">Future用户注册时间</h2>
@@ -42,43 +30,66 @@
             <div class="line"></div>
             <h2 class="userDes">备注</h2>
             <div class="note">
-            <textarea placeholder="点击添加备注" aria-label="备注" maxlength="256" class="textarea"
-                      style="height: 24px;"></textarea>
+                <textarea
+                    placeholder="点击添加备注"
+                    aria-label="备注"
+                    maxlength="256"
+                    class="textarea"
+                    style="height: 24px"
+                ></textarea>
             </div>
         </div>
         <div class="right-card">
             <div class="mutualGuildsList">
-                <button type="button"
-                        class="collapseBtn">
+                <button type="button" class="collapseBtn">
                     <div class="contents">
-                        <div class="defaultColor">
-                            16 个共同服务器中
-                        </div>
+                        <div class="defaultColor">16 个共同服务器中</div>
                         <div class="iconWrapper">
-                            <svg class="transition" width="24" height="24"
-                                 viewBox="0 0 24 24" aria-hidden="true" role="img">
-                                <path fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                      stroke-linejoin="round" d="M7 10L12 15 17 10" aria-hidden="true">
-                                </path>
+                            <svg
+                                class="transition"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                aria-hidden="true"
+                                role="img"
+                            >
+                                <path
+                                    fill="none"
+                                    stroke="currentColor"
+                                    stroke-width="2"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M7 10L12 15 17 10"
+                                    aria-hidden="true"
+                                ></path>
                             </svg>
                         </div>
                     </div>
                 </button>
             </div>
-            <br>
+            <br />
             <div class="mutualGuildsList">
-                <button type="button"
-                        class="collapseBtn">
+                <button type="button" class="collapseBtn">
                     <div class="contents">
-                        <div class="defaultColor">
-                            7 个共同好友
-                        </div>
+                        <div class="defaultColor">7 个共同好友</div>
                         <div class="iconWrapper">
-                            <svg class="transition" width="24" height="24"
-                                 viewBox="0 0 24 24" aria-hidden="true" role="img">
-                                <path fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                      stroke-linejoin="round" d="M7 10L12 15 17 10" aria-hidden="true">
-                                </path>
+                            <svg
+                                class="transition"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                aria-hidden="true"
+                                role="img"
+                            >
+                                <path
+                                    fill="none"
+                                    stroke="currentColor"
+                                    stroke-width="2"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M7 10L12 15 17 10"
+                                    aria-hidden="true"
+                                ></path>
                             </svg>
                         </div>
                     </div>
@@ -89,9 +100,9 @@
 </template>
 
 <script setup lang="ts">
-import {useUserFriendsStore} from '@/store/modules/friends';
+import { useUserFriendsStore } from '@/store/modules/friends';
 import router from '@/router';
-import {ElMessage} from "element-plus";
+import { ElMessage } from 'element-plus';
 
 const friendsStore = useUserFriendsStore();
 // 用户信息在friendsStore.friendInfo中，直接调用即可
@@ -101,17 +112,13 @@ watch(
     (id) => {
         friendsStore.getFriendInfo(id as string);
     },
-    {immediate: true}
+    { immediate: true }
 );
 const openVn = () => {
     ElMessage({
-        message: h('p', null, [
-            h('i',
-                {style: 'color:green'},
-                'Future-Support-会员标记')
-        ])
-    })
-}
+        message: h('p', null, [h('i', { style: 'color:green' }, 'Future-Support-会员标记')])
+    });
+};
 </script>
 
 <style lang="scss" scoped>
@@ -125,7 +132,7 @@ const openVn = () => {
     color: var(--text-normal);
 }
 
-.collapseBtn{
+.collapseBtn {
     background-color: transparent;
     border-radius: 0;
     height: 28px;
@@ -133,7 +140,7 @@ const openVn = () => {
     width: 90%;
 }
 
-.iconWrapper{
+.iconWrapper {
     color: var(--text-normal);
     justify-content: right;
     -ms-flex-item-align: right;
@@ -279,5 +286,4 @@ const openVn = () => {
     resize: none;
     width: 100%;
 }
-
 </style>
